b59ab8fb1a6f862bef8e180d0a4b4ebe9e4d2404
vikas pundir <vikaspundiruser@gmai.com>
2022-07-30 03:14:20 +0530

    Components divided

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header"
+import AddContact from "./AddContact"
+import ContactList from "./ContactList"
+
+
+
+function App() {
+  const contacts = [
+    {
+      id:"1",
+      name:"VIKAS",
+      email : "vikaspundiruser@gmail.com"
+    },
+    {
+      id:"2",
+      name:"pundir",
+      email : "pundiruser@gmail.com"
+    }
+  ]
+
+
+  return (
+    <div className='ui container'>
+      <Header/>
+      <AddContact/>
+      <ContactList contacts={contacts}/>
+    </div>
+  );
+}
+
+export default App;
